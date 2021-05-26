b968dd4ff05ed4167f72de4af1725a365ecad7ea
rubenfr14 <rubenfr14@hotmail.com>
2021-05-26 14:25:27 -0500

    VSDR 26-05

--- SCORM1/Controllers/VSDRController.cs
+++ SCORM1/Controllers/VSDRController.cs
@@ -18,6 +18,7 @@ using SCORM1.Models.ViewModel;
 using SCORM1.Models.VSDR;
 using System.Collections.Generic;
 using System;
+using System.Threading.Tasks;
 
 namespace SCORM1.Controllers
 {
@@ -54,8 +55,8 @@ namespace SCORM1.Controllers
         {
             VsdrUserVM vsdrModel = new VsdrUserVM();
             //Get list from data base;
-            List<VsdrSession> vsdrList= new List<VsdrSession>();
-            List<VsdrSession> tempList= new List<VsdrSession>();
+            List<VsdrSession> vsdrList = new List<VsdrSession>();
+            List<VsdrSession> tempList = new List<VsdrSession>();
             vsdrList = ApplicationDbContext.VsdrSessions.ToList();
 
             //Removes not available and date expired vsdr sessions To-Do
@@ -91,7 +92,7 @@ namespace SCORM1.Controllers
             VsdrSession vsdrToReturn = ApplicationDbContext.VsdrSessions.Find(id);
             List<VsdrUserFile> vsdrUserIssuedFiles = ApplicationDbContext.VsdrUserFiles.Where(x => x.user_id == actualUser.Id).ToList();
             List<VsdrTeacherComment> vsdrTeacherComments = ApplicationDbContext.VsdrTeacherComments.Where(x => x.user_id == actualUser.Id).ToList();
-            VsdrUserFile file = new VsdrUserFile(); 
+            VsdrUserFile file = new VsdrUserFile();
             if (vsdrToReturn.end_date.Subtract(DateTime.Now).TotalMinutes < 15)
             {
                 vsdrModel.meetingAvailable = true;
@@ -117,7 +118,7 @@ namespace SCORM1.Controllers
                 string[] allowedExtensions = new[] { ".pdf", ".doc", ".pptx", ".xls", ".xlsx", ".docx" };
                 var ext = Path.GetExtension(DateTime.Now.ToString("yyyyMMddHHmmss") + "-" + upload.FileName).ToLower();
                 var file = "";
-                foreach(var extention in allowedExtensions)
+                foreach (var extention in allowedExtensions)
                 {
                     if (extention.Contains(ext))
                     {
@@ -157,11 +158,284 @@ namespace SCORM1.Controllers
             VsdrSession vsdrToReturn = ApplicationDbContext.VsdrSessions.Find(id);
             return Redirect(vsdrToReturn.resource_url);
         }
-        
+
         #endregion
 
-        #region VSDR Admin
+        #region VSDR Admin        
         #endregion
+
+        public ActionResult CreateVSDR()
+        {
+            CreateVsdrSession vsdrModel = new CreateVsdrSession();
+            //Get list from data base;
+            List<VsdrSession> vsdrList = new List<VsdrSession>();
+            vsdrList = ApplicationDbContext.VsdrSessions.ToList();
+
+            //To-Do, filter list by enrollment and open
+            vsdrModel.listOfVsdr = vsdrList;
+            vsdrModel.Sesion = GetActualUserId().SesionUser;
+            return View(vsdrModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult CreateVSDR2(CreateVsdrSession entrada)
+        {
+            if (ModelState.IsValid)
+            {
+                var anterior = ApplicationDbContext.VsdrSessions.Where(x => x.name == entrada.actualVsdr1.name).Count();
+                if (anterior != 0)
+                {
+                    throw new Exception("El nombre de VSDR ya esta siendo utilizado ");
+                }
+
+                VsdrSession vsdrSession = new VsdrSession
+                {
+                    name = entrada.actualVsdr1.name,
+                    resource_url = entrada.actualVsdr1.resource_url,
+                    start_date = entrada.actualVsdr1.start_date,
+                    end_date = entrada.actualVsdr1.end_date,
+                    case_content = entrada.actualVsdr1.case_content,
+                    available = entrada.actualVsdr1.available,
+                    open = entrada.actualVsdr1.open,
+                };
+                ApplicationDbContext.VsdrSessions.Add(vsdrSession);
+                ApplicationDbContext.SaveChanges();
+
+            }
+            entrada.Sesion = GetActualUserId().SesionUser;
+            return RedirectToAction("CreateVSDR", "VSDR");
+        }
+        public ActionResult MatriculaVSDR(int? page, int id_vsdr)
+        {
+            int companyId = (int)GetActualUserId().CompanyId;
+            IPagedList<ApplicationUser> ListOfUser = ApplicationDbContext.Users.OrderBy(x => x.UserName).Where(x => x.CompanyId == companyId && x.ComunidadActiva != null).ToList().ToPagedList(page ?? 1, 15);
+            var actualVsdrEnrrollment = ApplicationDbContext.VsdrSessions.Where(x => x.id == id_vsdr).FirstOrDefault();
+
+            CreateVsdrSession model = new CreateVsdrSession
+            {
+                UserOfCompany = ListOfUser,
+                Id_VSDR = id_vsdr,
+
+            };
+            model.Sesion = GetActualUserId().SesionUser;
+            return View(model);
+        }      
+        [Authorize]
+        public ActionResult VSDR2(string id, int id_vsdr)
+        {
+            var user = id; 
+            var anterior = ApplicationDbContext.VsdrEnrollments.Where(x => x.user_id == user && x.vsdr_id == id_vsdr).FirstOrDefault();
+            if (anterior != null)
+            {
+                throw new Exception("Ya esta Matriculado");
+            }
+            var actualVsdrEnrrollment = ApplicationDbContext.VsdrSessions.Where(x => x.id == id_vsdr).FirstOrDefault();
+
+
+            VsdrEnrollment vsdrEnrollment = new VsdrEnrollment
+            {
+                user_id = user,
+                vsdr_id = id_vsdr,
+                vsdr_enro_init_date = actualVsdrEnrrollment.start_date,
+                vsdr_enro_finish_date = actualVsdrEnrrollment.end_date,
+                qualification = 0,
+            };
+
+
+            ApplicationDbContext.VsdrEnrollments.Add(vsdrEnrollment);
+            ApplicationDbContext.SaveChanges();
+            int a = 1;
+            return RedirectToAction("MatriculaVSDR", new { a, id_vsdr });
+        }
+        public ActionResult DeleteVSDR(string id, int id_vsdr)
+        {
+            var user = id;
+            var entidad = ApplicationDbContext.VsdrEnrollments.Where(x => x.user_id == user && x.vsdr_id == id_vsdr).FirstOrDefault();
+            if (entidad == null)
+            {
+                throw new Exception("No esta matriculado");
+            }         
+            ApplicationDbContext.VsdrEnrollments.Remove(entidad);
+            ApplicationDbContext.SaveChanges();
+            int a = 1;
+            return RedirectToAction("MatriculaVSDR", new { a, id_vsdr });
+        }
+        [Authorize]
+        public JsonResult DeleteSession(int id)
+        {
+            bool result = false;
+            var entidad = ApplicationDbContext.VsdrSessions
+                .Where(x =>x.id == id)
+                .FirstOrDefault();
+            var entidadMatriculas = ApplicationDbContext.VsdrEnrollments
+                .Where(x => x.vsdr_id == id)
+                .ToList();
+            if (entidad != null)
+            {
+                try
+                {
+                    ApplicationDbContext.VsdrEnrollments.RemoveRange(entidadMatriculas);
+                    ApplicationDbContext.VsdrSessions.Remove(entidad);                    
+                    ApplicationDbContext.SaveChanges();
+                    result = true;
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("El elemento no se puede ser eliminado",ex.InnerException);
+                }
+            }
+            else 
+            {
+                throw new Exception("No se encuentra VSDR");
+            }
+           
+                    
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }   
+        [Authorize]
+        public ActionResult UpdateSession(int id) 
+        {
+            CreateVsdrSession model = new CreateVsdrSession();
+            model.actualVsdr1 = new VsdrSession();
+            var entidad = ApplicationDbContext.VsdrSessions.Find(id);
+
+                if (entidad != null)
+                {
+                    model.actualVsdr1.id = entidad.id;
+                    model.actualVsdr1.name = entidad.name;
+                    model.actualVsdr1.open = entidad.open;
+                    model.actualVsdr1.start_date = entidad.start_date;
+                    model.actualVsdr1.end_date = entidad.end_date;
+                    model.actualVsdr1.available = entidad.available;
+                    model.actualVsdr1.case_content = entidad.case_content;
+                model.actualVsdr1.resource_url = entidad.resource_url;
+                   model.Sesion = GetActualUserId().SesionUser;
+                }
+                else
+                {
+                    throw new Exception("Error al encontrar VSDR");
+                }
+            return View(model);
+        }
+        [Authorize]
+        public ActionResult UpdateSession2(CreateVsdrSession entrada) 
+        {
+            var anterior = ApplicationDbContext.VsdrSessions
+                  .Where(x => x.id != entrada.actualVsdr1.id && x.name == entrada.actualVsdr1.name)
+                  .FirstOrDefault();
+
+            if (anterior != null)
+            {
+                throw new Exception("La configuracion ya existe");
+            }
+            var entidad = ApplicationDbContext.VsdrSessions
+                .Find(entrada.actualVsdr1.id);
+
+            if (entidad != null)
+            {
+
+                entidad.name = entrada.actualVsdr1.name;
+                entidad.case_content = entrada.actualVsdr1.case_content;
+                entidad.start_date = entrada.actualVsdr1.start_date;
+                entidad.end_date = entrada.actualVsdr1.end_date;
+                entidad.available = entrada.actualVsdr1.available;
+                entidad.open = entrada.actualVsdr1.available;
+                entidad.resource_url = entrada.actualVsdr1.resource_url;                
+                try
+                {
+                    entrada.Sesion = GetActualUserId().SesionUser;
+                    ApplicationDbContext.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("El elemento no se puede modificar", ex.InnerException);
+                }
+
+            }
+            else
+            {
+                throw new Exception("Elemento no encontrado");
+            }
+
+            return RedirectToAction("CreateVSDR");
+        }
+        public string IpUser()
+        {
+            string ip = Request.UserHostAddress;
+            string szXForwardedFor = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            string Ipserver;
+            if (szXForwardedFor != null)
+            {
+                Ipserver = szXForwardedFor;
+            }
+            else
+            {
+                Ipserver = ip;
+            }
+            return Ipserver;
+        }
+
+        public ActionResult SearchUserManager(UserAndMassiveManagementViewModel model, int? page)
+        {
+            var companyId = GetActualUserId().CompanyId;
+            if (string.IsNullOrEmpty(model.User_Id) || string.IsNullOrWhiteSpace(model.User_Id))
+            {
+                var table = ApplicationDbContext.TableChanges.Find(72);
+                var UserCurrent = ApplicationDbContext.Users.Find(GetActualUserId().Id);
+                var code = ApplicationDbContext.CodeLogs.Find(252);
+                var idcompany = UserCurrent.CompanyId;
+                if (idcompany != null)
+                {
+                    var company = ApplicationDbContext.Companies.Find(idcompany);
+                    string ip = IpUser();
+                    var idchange = new IdChange
+                    {
+                        IdCh_IdChange = null
+                    };
+                    ApplicationDbContext.IdChanges.Add(idchange);
+                    ApplicationDbContext.SaveChanges();
+                  
+                }
+                return RedirectToAction("MatriculaVSDR", new { page });
+            }
+            else
+            {
+                IPagedList<ApplicationUser> ListOfUser;
+                var conteo = ApplicationDbContext.Users.OrderBy(x => x.UserName).Where(x => x.CompanyId == companyId && (x.FirstName.Contains(model.User_Id) || x.LastName.Contains(model.User_Id) || x.UserName.Contains(model.User_Id))).ToList();
+                if (conteo.Count > 0)
+                {
+                    ListOfUser = ApplicationDbContext.Users.OrderBy(x => x.UserName).Where(x => x.CompanyId == companyId && (x.FirstName.Contains(model.User_Id) || x.LastName.Contains(model.User_Id) || x.UserName.Contains(model.User_Id))).ToList().ToPagedList(page ?? 1, conteo.Count);
+                }
+                else
+                {
+                    ListOfUser = ApplicationDbContext.Users.OrderBy(x => x.UserName).Where(x => x.CompanyId == companyId && (x.FirstName.Contains(model.User_Id) || x.LastName.Contains(model.User_Id) || x.UserName.Contains(model.User_Id))).ToList().ToPagedList(page ?? 1, 6);
+                }
+
+                model.Sesion = GetActualUserId().SesionUser;
+                var table = ApplicationDbContext.TableChanges.Find(72);
+                var UserCurrent = ApplicationDbContext.Users.Find(GetActualUserId().Id);
+                var code = ApplicationDbContext.CodeLogs.Find(252);
+                var idcompany = UserCurrent.CompanyId;
+                if (idcompany != null)
+                {
+                    var company = ApplicationDbContext.Companies.Find(idcompany);
+                    string ip = IpUser();
+                    var idchange = new IdChange
+                    {
+                        IdCh_IdChange = null
+                    };
+                    ApplicationDbContext.IdChanges.Add(idchange);
+                    ApplicationDbContext.SaveChanges();             
+                    
+                }
+                return View("MatriculaVSDR", model);
+            }
+        }
+
+
+
 
     }
 }
